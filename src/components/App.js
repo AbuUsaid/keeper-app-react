8c23389746d75b4d1724aab384070c79350ec82f
AbuUsaid <usaidabu321@gmail.com>
2023-08-14 14:44:54 +0530

    modify: map data from notes.js file to components to make it dynamic

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
+
+function createNoteItem(noteItem) {
+  return (
+    <Note
+      key={noteItem.key}
+      title={noteItem.title}
+      content={noteItem.content}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {/* <Note title="This is title" content="This is content" /> */}
+
+      {notes.map(createNoteItem)}
       <Footer />
     </div>
   );
